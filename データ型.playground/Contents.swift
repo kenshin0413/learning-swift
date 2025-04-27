1f62035db8ca38ec9261c930c65e273d3e8520de
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-04-27 23:45:09 +0900

    コメント

---
+++ データ型.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// var a = 1  } 数値
+                  
+// var b = 3.14 } 数値
+
+// var c = "こんにちは" } 文字列
+
+// 　　　　　型
+
+// Int .. 整数　var a = 10
+
+// Float .. 少数 var b = 3.14
+
+// Douible ..　少数
+
+// String .. 文字列 var c = "こんにちは"
+
+// Bool ..　真理値　true / false
+
+// var 変数名　: 型名　＝　値
+
+// let 定数名　:　型名　＝　値
+
+// var num : Int
+
+// let value : Float = 3.14
+
+var x = 10
+
+let y = 3.14
+
+let z = "こんにちは"
+
+var w = false
+
+print(type(of: x))
+
+print(type(of: y))
+
+print(type(of: z))
+
+print(type(of: w))
+
+var num : Float = 3.14
+
+num = 1.234
