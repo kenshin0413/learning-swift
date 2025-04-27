10c87bbc3f63af116ee81d47444d7f447e86a7ad
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-04-27 23:01:24 +0900

    コメント

--- 定数.playground/Contents.swift
+++ 定数.playground/Contents.swift
@@ -2,4 +2,35 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-print(greeting)
+// 変数..値変更可　定数..値変更不可
+// var..変数をつくれ　let..定数をつくれ
+// var aplle = 10
+// let orenge = "オレンジ"
+
+var a = 100
+
+print(a)
+
+a = 300
+
+print(a)
+
+let x = 100
+
+print(x)
+
+var num = 100
+
+print(num)
+
+var phrase = "これは変数です"
+
+print(phrase)
+
+num = 300
+
+print(num)
+
+phrase = "変数の値は変更できます"
+
+print(phrase)
