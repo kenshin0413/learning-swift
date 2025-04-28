76817b403b42bf13fc62e2929b0aad65123b0363
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-04-29 01:20:04 +0900

    m

---
+++ 数値化.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print(Float.pi)
+
+print(Double.pi)
+
+var a = 3.14
+
+print(type(of: a))
+
+var b = 1e5
+
+print(type(of: b))
+
+var c = 1e-3
+
+print(type(of: c))
+
+var intnum: Int
+
+intnum = 100_000
+
+let fnum: Float
+
+fnum = 1.23e-5
