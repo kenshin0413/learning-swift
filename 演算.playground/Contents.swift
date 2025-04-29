0522201472e97f970361a50f4749a4fd2db953bb
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-04-30 00:40:08 +0900

    m

---
+++ 演算.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// 算術演算子
+
+// +　足
+// -　引
+// * かけ
+// / 割
+// %　剰余(余りが表示される)
+
+// let three = 3
+// let four = 4
+// let seven = three + four
+// print(seven)
+
+// Int / Int = Int
+// 5 / 2 = 2  小数点は省かれる
+
+let a = 9
+let b = 7
+print(a % b)
+
+let i: Int = 10
+let j: Int = 3
+var result: Int
+result = i + j
+print(result)
+result = i - j
+print(result)
+result = i * j
+print(result)
+result = i / j
+print(result)
+result = i % j
+print(result)
+
+
+
+
+
+
+
+
