825ac2ff7d8573bd862b0434b7264b0b9fb45e55
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-05-01 00:41:56 +0900

    m

---
+++ 論理演算子.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var flag1 = true
+
+var flag2 = !flag1
+
+print(flag1,flag2)
+
+let a1 = 2.0
+
+let b1 = 3.5
+
+var Bool1 = a1 > b1
+
+print(Bool1)
+
+var result = flag1 && flag2  //論理積 a && b  a かつ b
+print(result)
+
+result = flag1 || flag2  //論理和 a || b  a または b
+print(result)
