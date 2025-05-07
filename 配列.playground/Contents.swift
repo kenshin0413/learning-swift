de916f0feb815099809a4f28a4aa4c503911d352
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-05-07 21:22:04 +0900

    m

---
+++ 配列.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// 配列型・・・データを並べたもの
+
+// リテラル・・・[値,値,値]
+// var a = [2,4,6,8]
+
+//          0,1,2,3,4  ←　インデックス番号
+// var a = [0,2,4,6,8]
+// print(a[3])  6と出力される
+// var i = 2
+// print(a[2 * i])   計算することもできる
+
+// 配列の結合
+
+let iroha = ["い","ろ","は"]
+let nihoheto = ["に","ほ","へ","と"]
+let arry = iroha + nihoheto
+print(arry)
+
