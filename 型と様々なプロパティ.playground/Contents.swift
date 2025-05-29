213b05b5e1fc7b896ecd9c551cf38ec94d70ea0a
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-05-29 20:12:29 +0900

    型と様々なプロパティ

---
+++ 型と様々なプロパティ.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//  構造体....struct
+//  クラス....class
+//  列挙型....enum
+
+// コンピューテッドプロパティ
+// 値を自身に持たないプロパティ
+// 基本の書式
+// var 変数名 : 型名 {
+//    get {
+//        処理
+//        return 戻り値
+//    }
+//    set(引数名) {
+//        処理
+//    }
+// }
